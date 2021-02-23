9a3f3153058e547ec7cd0dd96865065f16555e50
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-23 01:52:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,7 +32,7 @@ class CashRegister
   def items
     item_names = []
     @cart.each do |item_info|
-      for qty in 1..item_info[:quantity] m
+      for qty in 1..item_info[:quantity] 
       item_names << item_info[:name]
       end
     end
